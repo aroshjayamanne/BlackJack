4ebe1061a685c901dcaa976c760e2881684d7cf2
Arosh Jayamanna <aroshjayamanne@gmail.com>
2021-12-20 22:08:36 +0530

    blackjack message implemented

--- scripts.js
+++ scripts.js
@@ -1,24 +1,35 @@
-let firsttCard = 10
+let firsttCard = 11
 let secondCard = 11
 let sum = firsttCard + secondCard
 let hasBlackJack = false
 let amountWon = 0
 let isAlive = true
 let message = ''
+let text = document.getElementById('sum-el')
 
-if (sum <= 20) {
-    message = 'Do you want to draw a new card? '
+function startGame() {
+    if (sum <= 20) {
+        message = 'Do you want to draw a new card? '
+    }
+    
+    else if ( sum === 21) {
+        message = 'U got a blackjack'
+        amountWon += 10
+        hasBlackJack = true
+        
+    }
+    
+    else {
+        message = 'You are out of the game'
+        isAlive = false
+    }
+    
+    let text = document.getElementById('message-el')
+    text.textContent = message
+    
+    let cards = document.getElementById('cards-el')
+    cards.textContent = firsttCard + ' & ' + secondCard
+
+    let summ = document.getElementById('sum-el')
+    summ.textContent =  summ.textContent + (firsttCard + secondCard) + ' '
 }
-
-else if ( sum === 21) {
-    message = 'U got a blackjack'
-    amountWon += 10
-    hasBlackJack = true
-}
-
-else {
-    message = 'You are out of the game'
-    isAlive = false
-}
-
-console.log (message)
